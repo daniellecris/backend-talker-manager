cffd102d034307cc98e7efc5807e8580dee42c08
Danielle Cristina <dnaielle.trybe@gmail.com>
2022-05-23 22:46:08 -0300

    Requisito 2

--- index.js
+++ index.js
@@ -13,14 +13,24 @@ app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
 
-// app.use('/', require('./routes/index'));
-
 // app.get('/test', (req, res) => res.status(200).json({ message: 'Rota teste' }));
 
 app.get('/talker', async (_req, res) => {
   const talker = await readFile();
 
   return res.status(200).json(talker);
+});
+
+app.get('/talker/:id', async (req, res) => {
+  const talker = await readFile();
+  const { id } = req.params;
+  
+  const result = talker.find((t) => t.id === +id);
+
+  if (!result) {
+    return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+  }
+  return res.status(200).json(result);
 });
 
 app.listen(PORT, () => {
